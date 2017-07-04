8bcda96b146b58969f3500cd0913d329ce7ca74c
Philipe <philipe40@hotmail.com>
2017-07-04 17:33:25 -0300

    Aplicação finalizada, com exceção de autenticação.

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,23 +1,54 @@
 (function() {
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
     '$http',
+    '$location',
     'msgs',
     'tabs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http, msgs, tabs) {
+  function BillingCycleController($http, $location, msgs, tabs) {
     const vm = this
     const url = 'http://localhost:3003/api/billingCycles'
 
+    /**************************
+    ***VISUALIZAÇÃO DAS TABS***
+    **************************/
+
+    //Visualização da tab Alterar
+    vm.showTabUpdate = function (billingCycle) {
+      vm.billingCycle = billingCycle
+      vm.calculateValues()
+      tabs.show(vm, {tabUpdate: true})
+    }
+
+    //Visualização da tab Excluir
+    vm.showTabDelete = function (billingCycle) {
+      vm.billingCycle = billingCycle
+      vm.calculateValues()
+      tabs.show(vm, {tabDelete: true})
+    }
+
+    /**************************
+    ***********CRUD************
+    **************************/
+
+    //Listar
     vm.refresh = function() {
-      $http.get(url).then(function(response) {
+      const page = parseInt($location.search().page) || 1
+      $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response) {
         vm.billingCycle = {credits: [{}], debts: [{}]}
         vm.billingCycles = response.data
-        tabs.show(vm, {tabList: true, tabCreate: true})
+        vm.calculateValues()
+
+        $http.get(`${url}/count`).then(function (response) {
+          vm.pages = Math.ceil(response.data.value / 10)
+          tabs.show(vm, {tabList: true, tabCreate: true})
+        })
       })
     }
 
+    //Salvar
     vm.create = function() {
       $http.post(url, vm.billingCycle).then(function(response) {
         vm.refresh()
@@ -27,16 +58,7 @@
       })
     }
 
-    vm.showTabUpdate = function (billingCycle) {
-      vm.billingCycle = billingCycle
-      tabs.show(vm, {tabUpdate: true})
-    }
-
-    vm.showTabDelete = function (billingCycle) {
-      vm.billingCycle = billingCycle
-      tabs.show(vm, {tabDelete: true})
-    }
-
+    //Alterar
     vm.update = function () {
       const updateUrl = `${url}/${vm.billingCycle._id}`
       $http.put(updateUrl, vm.billingCycle).then(function (response) {
@@ -47,6 +69,7 @@
       })
     }
 
+    //Excluir
     vm.delete = function () {
       const deleteUrl = `${url}/${vm.billingCycle._id}`
       $http.delete(deleteUrl, vm.billingCycle).then(function (response) {
@@ -57,32 +80,72 @@
       })
     }
 
+    /**************************
+    *********CRÉDITOS**********
+    **************************/
+
+    //Adiciona Crédito
     vm.addCredit = function (index) {
       vm.billingCycle.credits.splice(index + 1, 0, {})
     }
 
+    //Clona Crédito
     vm.cloneCredit = function (index, {name, value}) {
       vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+      vm.calculateValues()
     }
 
+    //Exclui Crédito
     vm.deleteCredit = function (index) {
       if (vm.billingCycle.credits.length > 1) {
         vm.billingCycle.credits.splice(index, 1)
+        vm.calculateValues()
       }
     }
 
+    /**************************
+    **********DÉBITOS**********
+    **************************/
+
+    //Adiciona Débito
     vm.addDebt = function (index) {
       vm.billingCycle.debts.splice(index + 1, 0, {})
     }
 
+    //Clona Débito
     vm.cloneDebt = function (index, {name, value, status}) {
       vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+      vm.calculateValues()
     }
 
+    //Exclui Débito
     vm.deleteDebt = function (index) {
       if (vm.billingCycle.debts.length > 1) {
         vm.billingCycle.debts.splice(index, 1)
+        vm.calculateValues()
       }
+    }
+
+    /**************************
+    **********SUMÁRIO**********
+    **************************/
+
+    //Calcula os valores do sumário
+    vm.calculateValues = function () {
+      vm.credit = 0
+      vm.debt = 0
+
+      if (vm.billingCycle) {
+        vm.billingCycle.credits.forEach(function ({value}) {
+          vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+
+        vm.billingCycle.debts.forEach(function ({value}) {
+          vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+      }
+
+      vm.total = vm.credit - vm.debt
     }
 
     vm.refresh()
